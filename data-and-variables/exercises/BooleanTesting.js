2ca21effef06e16c9a047d8479dc6943b8f10d72
Renou chea <renoucheausa@gmail.com>
2024-01-10 19:49:46 -0600

    conditional excercise

---
+++ data-and-variables/exercises/BooleanTesting.js
@@ -0,0 +1,30 @@
+//console.log(Boolean("true"));
+//console.log(Boolean("TRUE"));
+//console.log(Boolean(0));
+//console.log(Boolean(1));
+//console.log(Boolean(''));
+//console.log(Boolean('LaunchCode'));
+
+//let a = 7;
+//if (a % 2 === 1) {
+   //console.log("Launch");
+//} else if (a > 5) {
+   //console.log("Code");
+//} else {
+  // console.log("LaunchCode");
+//}
+
+let num = 0;
+console.log (num);
+
+//let foo = (num % 2);
+//console.log(foo);
+
+if (num % 2 === 0) {
+    if (num % 2 ===1) {
+        console.log("odd");
+    }
+}
+else {
+    console.log("we");
+}
