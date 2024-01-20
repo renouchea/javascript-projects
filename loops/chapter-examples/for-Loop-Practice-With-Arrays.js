0c77b8c55e3cc3165e63e9948970092e6ae037a7
Renou chea <renoucheausa@gmail.com>
2024-01-20 10:16:28 -0600

    loop practice

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let nameall = ["Renou","Rotha","Mey","Rothana"];
 // write a for loop that prints each name on a different line
+for (let i=0;i<nameall.length;i++){
+    console.log(nameall[i]);
+}
